c52e63e48551375d5eb4714cfcb90c93e06875bf
Oleksii Ablitsov <oleksii.ablitsov@bringg.com>
2023-10-16 15:15:11 +0300

    task 4

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonElem = document.querySelector("button.change-color");
+const bodyElem = document.querySelector("body");
+
+buttonElem.addEventListener("click", () => {
+  bodyElem.style.backgroundColor = getRandomHexColor();
+});

9d6e5f55697f5434005ab8bcedcae3cf463c8efa
Oleksii Ablitsov <oleksii.ablitsov@bringg.com>
2023-10-16 15:12:39 +0300

    task 3

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formElem = document.querySelector("form.login-form");
 
+formElem.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const { email, password } = formElem.elements;
+
+  const formData = {
+    email: email.value.trim(),
+    password: password.value.trim(),
+  };
+
+  for (const value of Object.values(formData)) {
+    if (!value) {
+      alert(`All fields must be filled`);
+      return;
+    }
+  }
+
+  console.log(formData);
+  formElem.reset();
+});
